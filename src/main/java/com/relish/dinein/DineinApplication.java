33b430a6525d3ec84c5d2e10f533031db2568f01
Nindiya31 <nindiyabhagat2016@gmail.com>
2026-04-29 15:07:19 +0530

    changes in console settings

--- src/main/java/com/relish/dinein/DineinApplication.java
+++ src/main/java/com/relish/dinein/DineinApplication.java
@@ -2,6 +2,8 @@ package com.relish.dinein;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class DineinApplication {
@@ -9,5 +11,10 @@ public class DineinApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DineinApplication.class, args);
 	}
+	
+	 @EventListener(ApplicationReadyEvent.class)
+	    public void onStart() {
+	        System.out.println("\n:::::: Relish Application started ::::::\n");
+	    }
 
 }
